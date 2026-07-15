a45439a016e91c6359ab370e98263a13baa5a641
MrInal-M3hta <mrinalmehta11@gmail.com>
2026-07-15 21:13:37 +0530

    Adding normal change

--- part2/09_null_and_is_not_null.sql
+++ part2/09_null_and_is_not_null.sql
@@ -2,18 +2,17 @@
 
 -- null - missing/unknown value
 -- u should not check null using = null
--- IS NULL
--- IS NOT NULL
+-- Instead use IS NULL or IS NOT NULL to check for null values.
 
 
--- SELECT name, description
--- FROM products
--- WHERE description IS NULL;
+SELECT name, description
+FROM products
+WHERE description IS NULL;
 
 
--- SELECT name, description
--- FROM products
--- WHERE description IS NOT NULL;
+SELECT name, description
+FROM products
+WHERE description IS NOT NULL;
 
 SELECT name, category, is_active, description
 FROM products
