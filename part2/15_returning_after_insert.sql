ffa9df5e95b43a2d0d5f90138a8dea6f16e12a73
MrInal-M3hta <mrinalmehta11@gmail.com>
2026-07-15 22:19:26 +0530

    Last Commit of part2

--- part2/15_returning_after_insert.sql
+++ part2/15_returning_after_insert.sql
@@ -2,15 +2,15 @@
 
 
 
--- INSERT INTO products (name, category, price, stock, sku, description)
--- VALUES ('webcam camera', 'electronics', 456.67, 56, 'ELEC-WEB-009', 'webcam camera description')
--- RETURNING id, name, category, price, stock, created_at;
+INSERT INTO products (name, category, price, stock, sku, description)
+VALUES ('webcam camera', 'electronics', 456.67, 56, 'ELEC-WEB-009', 'webcam camera description')
+RETURNING id, name, category, price, stock, created_at;
 
 
--- UPDATE products
--- SET stock = stock + 11
--- WHERE sku = 'ELEC-WEB-009'
--- RETURNING id, name, stock;
+UPDATE products
+SET stock = stock + 11
+WHERE sku = 'ELEC-WEB-009'
+RETURNING id, name, stock;
 
 DELETE FROM products
 WHERE sku = 'ELEC-WEB-009'
