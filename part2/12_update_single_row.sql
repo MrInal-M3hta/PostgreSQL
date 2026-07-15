7a8cdc8f11d59e5cb56b40526b963c51c272ccab
MrInal-M3hta <mrinalmehta11@gmail.com>
2026-07-15 21:43:26 +0530

    Adding notes on updating single row

--- part2/12_update_single_row.sql
+++ part2/12_update_single_row.sql
@@ -1,5 +1,30 @@
 
 
+-- UPDATE : is used to modify existing data in a table.
+
+/*
+Basic syntax of UPDATE statement is as follows:
+    UPDATE table_name
+    SET column_name = value1, column_name = value2, ...
+    WHERE condition;
+
+Think of it like this:
+
+    UPDATE
+    ↓
+    Which table?
+
+    SET
+    ↓
+    What do you want to change?
+
+    WHERE
+    ↓
+    Which row(s)?
+
+*/
+
+
 SELECT name, price, stock, sku
 FROM products
 WHERE sku = 'ELEC-KEY-001';
@@ -12,3 +37,5 @@ WHERE sku = 'ELEC-KEY-001';
 SELECT name, price, stock, sku
 FROM products
 WHERE sku = 'ELEC-KEY-001';
+
+-- ⚠️ Very Important: Never Forget WHERE
