a854a1302488e41935b1bb7ffe3da19f1693ec1f
MrInal-M3hta <mrinalmehta11@gmail.com>
2026-07-15 21:09:42 +0530

    Adding details little detail in IN, NOT IN and BETWEEN file

--- part2/08_in_not_in_between.sql
+++ part2/08_in_not_in_between.sql
@@ -1,20 +1,20 @@
 
--- IN -> value must match one item from the list 
--- NOT IN -> value must not match any time from the list 
--- BETWEEN -> value must be inside a range
+-- IN -> value must match one item from the list. OR “Return rows whose value is in this list.”
+-- NOT IN -> value must not match any time from the list. OR “Return rows whose value is NOT in this list.”
+-- BETWEEN -> value must be inside a range. OR “Return rows whose value is between this range.”
 
 
--- SELECT name, category, price 
--- FROM products
--- WHERE category IN ('stationery', 'furniture');
+SELECT name, category, price 
+FROM products
+WHERE category IN ('stationery', 'furniture');
 
--- SELECT name, category
--- FROM products
--- WHERE category NOT IN ('electronics', 'furniture');
+SELECT name, category
+FROM products
+WHERE category NOT IN ('electronics', 'furniture');
 
--- SELECT name, price
--- FROM products
--- WHERE price BETWEEN 100 AND 2000;
+SELECT name, price
+FROM products
+WHERE price BETWEEN 100 AND 2000; -- It will include 100 and 2000 as well.
 
 
 SELECT name, category, price, stock
