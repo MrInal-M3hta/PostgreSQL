b2f15eaf28e756eb3b9c69709d1608a5a58fe5e1
MrInal-M3hta <mrinalmehta11@gmail.com>
2026-07-15 21:17:23 +0530

    Adding normal change and adding defination of ORDER BY

--- part2/10_order_by.sql
+++ part2/10_order_by.sql
@@ -1,13 +1,14 @@
 
 
+-- ORDER BY -> “Return rows whose value is NOT in this list.”
 
--- SELECT name, price
--- FROM products
--- ORDER BY price ASC;
+SELECT name, price
+FROM products
+ORDER BY price ASC;
 
--- SELECT name, price
--- FROM products
--- ORDER BY price DESC;
+SELECT name, price
+FROM products
+ORDER BY price DESC;
 
 SELECT name, category, price
 FROM products
